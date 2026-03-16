a3acce6c4c959908cd4be2aad1ee80add67bd10e
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-16 15:52:33 +0100

    added seeding script

---
+++ python-cli/seeding.sql
@@ -0,0 +1,44 @@
+DELETE FROM centers;
+DELETE FROM rooms;
+DELETE FROM bikes;
+DELETE FROM users;
+DELETE FROM staff;
+DELETE FROM group_sessions;
+DELETE FROM instructor_for;
+
+UPDATE SQLITE_SEQUENCE SET seq = 0
+WHERE (
+	name = 'centers'
+	OR name = 'rooms'
+	OR name = 'users'
+	OR name = 'staff'
+);
+
+INSERT INTO centers (name, address, opening_time, closing_time)
+VALUES 	('Gløshaugen', 'Chr. Fredriks gate 20', '05:00', '23:59'),
+	('Moholt', 'Moholt almenning 12', '05:00', '23:59');
+
+INSERT INTO rooms (center_id)
+VALUES 	(1),
+	(1),
+	(2);
+
+INSERT INTO bikes (room_id, bodybike, number)
+VALUES	(1, false, 1),
+	(1, true, 2),
+	(2, false, 1);
+
+INSERT INTO users (first_name, last_name, mail, phone_number, member_in_club)
+VALUES	('Ola', 'Normann', 'ola.normann@mail.no', '12345678', NULL),
+	('Kari', 'Normann', 'kari.normann@mail.no', '87654321', NULL),
+	('Navn', 'Navnesen', 'navn.navnesen@mail.no', '00000000', NULL);
+
+INSERT INTO staff (first_name, last_name)
+VALUES	('Tre', 'Ner'),
+	('Ins', 'Truktør');
+
+INSERT INTO group_sessions (start_time, duration, activity, max_attendants, room_id, club_id, description)
+VALUES	('2026-10-12 18:00', 120, 'Spinning', 10, 1, null, 'Rommet har 2 sykler, vi bytter på.');
+
+INSERT INTO instructor_for (staff_id, session_time, session_room)
+VALUES	(1, '2026-10-12 18:00', 1);
