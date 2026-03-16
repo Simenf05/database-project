c85d07c2e05813dc148874f0ae9c67833e10ec5c
Simen Fritsvold <sifra002@gmail.com>
2026-03-16 15:22:02 +0100

    write cli parser for sitcli

---
+++ python-cli/sitcli.py
@@ -0,0 +1,34 @@
+#!/bin/env python3
+
+import sqlite3
+import sys
+
+def getUsers():
+
+    print("Noe")
+
+def noe():
+    print("Noe")
+
+def help():
+    print("""sitcli [COMMAND]""")
+
+
+def main():
+    argv = sys.argv
+
+    if len(argv) < 2:
+        help()
+        sys.exit()
+
+    command = argv[1]
+
+    match command:
+        case "noe":
+            noe()
+        case "get users":
+            noe()
+
+if __name__ == "__main__":
+    main()
+
