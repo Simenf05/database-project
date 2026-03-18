23f5ed73e0e5db6984e7464af17e6d0ed8f50123
Simen Fritsvold <sifra002@gmail.com>
2026-03-18 10:38:28 +0100

    Register for session in python

--- python-cli/sitcli.py
+++ python-cli/sitcli.py
@@ -11,22 +11,48 @@ def session_room_center_join():
     return "group_sessions AS g INNER JOIN rooms AS r ON (g.room_id = r.id) INNER JOIN centers AS c ON (r.center_id = c.id)"
 
 
+# Antagelse: Epost er brukernavn
+
 def register():
-    # username = input("Username: ")
-    activity = input("Activity: ")
-    # time = input("Time slot: ")
+    mail = "ola.normann@mail.no" # "johnny@stud.ntnu.no" # input("Mail: ")
 
-    query = f"SELECT * FROM {session_room_center_join()} WHERE g.activity LIKE ?"
+    if "%" in mail:
+        print("Not a valid mail.")
+        exit()
 
-    print(query)
+    activity = "Spinning" # input("Activity: ")
+    year_and_date = "2026-10-12" # input("Year and date (YYYY-MM-DD): ")
 
-    cursor.execute(query, (activity,))
-    rows = cursor.fetchall()
-    print(rows)
+    query = f"SELECT time(start_time), room_id, duration, start_time FROM {session_room_center_join()} WHERE g.activity LIKE ? AND date(g.start_time) = ?"
+
+    cursor.execute(query, (activity, year_and_date,))
+    sessions_at_date = cursor.fetchall()
+
+    print("Select your session:")
+
+    while True:
+        for i, session in enumerate(sessions_at_date):
+            print(f"{i + 1}: {session}")
+        try:
+            index = int(input()) - 1
+            selected = sessions_at_date[index]
+        except:
+            continue;
+        break
+
+    query = "SELECT id FROM users WHERE mail LIKE ?"
+    cursor.execute(query, (mail,))
+    user_id = cursor.fetchone()
+
+    insert_query = "INSERT INTO registered (session_time, session_room, user_id) VALUES (?, ?, ?)"
+    cursor.execute(insert_query, (selected[3], selected[1], user_id[0],))
+    print("Success")
 
 
 def help():
-    print("""sitcli [COMMAND]""")
+    print("""sitcli [COMMAND]
+[COMMAND]:
+    register""")
 
 
 def main():
