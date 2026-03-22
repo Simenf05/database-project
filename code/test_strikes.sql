24d8e3648957ade73cae50cfb6f59a417ecb737a
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-22 18:35:57 +0100

    update test strikes

--- code/test_strikes.sql
+++ code/test_strikes.sql
@@ -1,24 +1,9 @@
-INSERT INTO sports_clubs (name) VALUES ('Test Club');
-
-INSERT INTO centers (name, address, opening_time, closing_time) 
-VALUES ('Test Center', 'Street 1', '06:00', '22:00');
-
-INSERT INTO rooms (center_id) VALUES (1);
-
-INSERT INTO users (first_name, last_name, mail, member_in_club)
-VALUES ('Ola', 'Nordmann', 'ola.nordmann@example.com', 1);
-
 INSERT INTO strikes (session_time, session_room, user_id) 
 VALUES ('2026-03-01 10:00:00', 1, 1);
 INSERT INTO strikes (session_time, session_room, user_id) 
 VALUES ('2026-03-02 11:00:00', 1, 1);
 INSERT INTO strikes (session_time, session_room, user_id) 
 VALUES ('2026-03-03 12:00:00', 1, 1);
-
-INSERT INTO group_sessions (start_time, duration, activity, max_attendants, room_id, club_id, description)
-VALUES (
-    '2026-03-15 10:00:00', 60, 'Pilates', 10, 1, 1, 'Pilates session'
-);
 
 INSERT INTO registered (session_time, session_room, user_id)
 VALUES ('2026-03-15 10:00:00', 1, 1);
