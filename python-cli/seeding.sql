6735134501870e3ae41b09a4c02bb4c36508aa1c
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-22 12:20:24 +0100

    added 2 sessions in week 12

--- python-cli/seeding.sql
+++ python-cli/seeding.sql
@@ -40,10 +40,25 @@ INSERT INTO staff (first_name, last_name)
 VALUES	('Tre', 'Ner'),
 	('Ins', 'Truktør');
 
-INSERT INTO group_sessions (start_time, duration, activity, max_attendants, room_id, club_id, description)
-VALUES	('2026-03-19 18:00', 120, 'Spinn60', 10, 1, null, 'Rommet har 2 sykler, vi bytter på.'),
-	('2026-03-19 18:00', 120, 'Spinn70', 10, 2, null, 'Rommet har 2 sykler, vi bytter på.'),
-	('2026-03-29 18:00', 120, 'Spinn70', 10, 2, null, 'Rommet har 2 sykler, vi bytter på.');
+INSERT INTO group_sessions (start_time, duration, activity, max_attendants, room_id, creation_time, club_id, description)
+VALUES
+(
+    datetime(date('now', '+1 day') || ' 12:00'),
+    60, 'Spin60', 10, 1, date('now'), null,
+    'Rommet har 2 sykler, vi bytter på.'
+),
+(
+    datetime(date('now', '+1 day') || ' 18:00'),
+    60, 'Spin70', 10, 2, date('now'), null,
+    'Rommet har 2 sykler, vi bytter på.'
+),
+(
+    datetime(date('now', '+1 day') || ' 19:00'),
+    70, 'Spin70', 10, 2, date('now'), null,
+    'Rommet har 2 sykler, vi bytter på.'
+),
+('2026-03-17', 120, 'Spin120', 10, 1, '2026-02-17', null, '120 min spinning'),
+('2026-03-18', 120, 'Spin120-2', 10, 1, '2026-02-18', null, '120 min spinning igjen');
 
 INSERT INTO instructor_for (staff_id, session_time, session_room)
 VALUES	(1, '2026-10-12 18:00', 1);
