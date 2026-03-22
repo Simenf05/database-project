4c95e1821a76a1326268f06731a844da35bab686
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-22 11:20:58 +0100

    changed seeding to automatically add dates one day in future

--- python-cli/seeding.sql
+++ python-cli/seeding.sql
@@ -41,9 +41,22 @@ VALUES	('Tre', 'Ner'),
 	('Ins', 'Truktør');
 
 INSERT INTO group_sessions (start_time, duration, activity, max_attendants, room_id, club_id, description)
-VALUES	('2026-08-31 18:30', 120, 'Spinn60', 10, 1, null, 'Rommet har 2 sykler, vi bytter på.'),
-	('2026-08-31 18:00', 120, 'Spinn70', 10, 2, null, 'Rommet har 2 sykler, vi bytter på.'),
-	('2026-09-31 19:00', 120, 'Spinn70', 10, 2, null, 'Rommet har 2 sykler, vi bytter på.');
+VALUES
+(
+    datetime(date('now', '+1 day') || ' 18:30'),
+    120, 'Spinn60', 10, 1, null,
+    'Rommet har 2 sykler, vi bytter på.'
+),
+(
+    datetime(date('now', '+1 day') || ' 12:00'),
+    120, 'Spinn70', 10, 2, null,
+    'Rommet har 2 sykler, vi bytter på.'
+),
+(
+    datetime(date('now', '+1 day') || ' 19:00'),
+    120, 'Spinn70', 10, 2, null,
+    'Rommet har 2 sykler, vi bytter på.'
+);
 
 INSERT INTO instructor_for (staff_id, session_time, session_room)
 VALUES	(1, '2026-10-12 18:00', 1);
