a319409ff5b781fe972d9823bc3faa30da85360c
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-22 14:33:20 +0100

    added use case 8

---
+++ python-cli/training_pairs.sql
@@ -0,0 +1,26 @@
+SELECT
+	u1.mail AS mail1,
+	u2.mail AS mail2,
+	COUNT(*) AS occurences
+FROM (
+	users AS u
+	INNER JOIN attended AS a ON u.id = a.user_id
+) AS u1
+CROSS JOIN
+(
+	users AS u
+	INNER JOIN attended AS a ON u.id = a.user_id
+) AS u2
+WHERE (
+	u1.session_time = u2.session_time
+	AND u1.session_room = u2.session_room
+	AND u1.user_id < u2.user_id
+)
+GROUP BY
+	u1.user_id,
+	u1.first_name,
+	u1.last_name,
+	u1.user_id,
+	u2.first_name,
+	u2.last_name
+ORDER BY occurences DESC;
